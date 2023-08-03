19ff8c8eb3980073336fe6f3b7c10c1909b35d23
Anne-Marie <108076177+Aimes13@users.noreply.github.com>
2023-08-04 02:27:00 +0800

    Add getUserById and deleteUser functions

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,39 @@
 const Queries = require('../model/queries');
 
-const querySchema = {userId: ''};
+const querySchema = {name: 'users', userId: '', userDetails: ''};
 const userQueries = new Queries(querySchema);
+
+const getUserById = (req, res) => {
+    querySchema.userId = req.session.user.id;
+    userQueries.getUserById()
+    .then(data => {
+        if(!data.error){
+            res.send(data.rows);
+        } else {
+            res.send('User does not exist!');
+        };
+    });
+};
+
+/*const updateUser = (req, res) => {
+    querySchema.userId = req.session.user.id;
+    const { telephone, address } = req.body;
+    querySchema.userDetails = { telephone, address };
+    userQueries.updateUser()
+    .then(data => {
+        if(!data.error){
+            res.send('You have successfully updated your user information!');
+        } else {
+            res.send(data.errorMessage);
+        };
+    });
+}; */
+
+const deleteUser = (req, res) => {
+    querySchema.userId = req.session.user.id;
+    userQueries.deleteUser()
+    .then(data => res.send('You have successfully deleted your account!'));
+}
 
 const getOrderHistory = (req, res) => {
     querySchema.userId = req.session.user.id;
@@ -9,4 +41,9 @@ const getOrderHistory = (req, res) => {
     .then(data => res.send(data));
 };
 
-module.exports = {getOrderHistory};
+module.exports = {
+    getUserById,
+    //updateUser,
+    deleteUser,
+    getOrderHistory
+};
